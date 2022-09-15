39f729fea1c546c8804ba574cac70d7500706767
Gary Malcolm Bleasdale <gary@bleasdale.dev>
2022-09-15 17:44:42 -0300

    stylede navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import './navbar.scss'
+import { ReactComponent as CrownLogo} from '../../crown.svg'
 
 const Navbar =()=>{
   return (
     <>  
-      <h2>I am a navbar</h2>
+      <div className='navbar'>
+        <CrownLogo />
+         <div className='link-container'>
+          <a href='/'>SHOP</a>
+          <a href='/'>CONTACT</a>
+          <a href='/'>SIGN IN</a>
+         </div>
+      </div>
       <Outlet />
     </>
   )
